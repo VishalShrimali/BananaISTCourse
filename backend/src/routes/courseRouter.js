236501ca1fb66905f2e89fd4817261462b845e90
VishalShrimali <24vishalmali@gmail.com>
2025-02-04 15:15:25 +0530

    final commit

--- backend/src/routes/courseRouter.js
+++ backend/src/routes/courseRouter.js
@@ -2,109 +2,17 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import {Course} from "../models/course.models.js";
 import {courseAuth} from "../middleware/courseAuth.js";
+import { addCourse, deleteCourse, updateCourse, viewCourse } from "../controllers/courseController.js";
 
 const courseRoute = express.Router();
 
-courseRoute.post('/add',    async (req, res) => {
-    try{
-        let { title,  description, price, lessons, studentsEnrolled } = req.body;
-        const newcourse = await Course.create({title,  description, price, lessons, studentsEnrolled});
-        const token = newcourse.generateAuthToken();
-        return res.status(201).json({
-            message: "Course added succesfully",
-            token
-            
-        })
-    }
-    catch(error){
-        console.log("Error is : ",error);
-        res.status(500).json({
-            message: "Server Error."
-        })
-        
-    }
-});
+courseRoute.post('/add',  addCourse );
 
-courseRoute.get('/view/:id', courseAuth, async (req, res) => {
-    try{
-         const _id  = req.params.id;
-                  // Find user in a single query
-                const course = await Course.findById(_id);
-        
-                if (!course) {
-                    return res.status(401).json({ message: "Course does not exist." });
-                }
-        
-                 res.status(201).json({
-                    message: "Course Details",
-                    title: course.title,
-                    description: course.description
-                 })
-    }
-    catch(error){
-        console.log("Error is : ",error);
-        res.status(500).json({
-            message: "Server Error."
-        })
-        
-    }
-});
+courseRoute.get('/view/:id', courseAuth, viewCourse );
 
-courseRoute.put('/update/:id', courseAuth, async (req, res) => {
-  try{
-        const { title, description, price } = req.body;
+courseRoute.put('/update/:id', courseAuth, updateCourse);
 
-        const existingCourse = await Course.findOne({title});
-        if(existingCourse){
-            return res.status(400).json({ message: "Course already exists." });
-        };
-
-        if(!title || !description || !price) {
-            return res.status(400).json({ message: "All fields are mandatory." });
-        }
-        const newcourse = await Course.findOneAndUpdate({ title, description, price  });
-        res.status(201).json(
-            {
-                message: "Course Updated successfully.",
-                name: newcourse.name
-            }
-        );
-        
-    }
-    catch(error){
-        console.log("Error: ",error);
-        res.status(500).json({ message: "Internal Server Error", error: error.message });
-
-        
-    }
-        
-    
-});
-
-courseRoute.delete('/remove/:id', courseAuth, async (req, res) => {
-    try{
-          const _id = req.params.id
-          const course = await Course.findByIdAndDelete(_id);
-        
-                if (!course) {
-                    return res.status(401).json({ message: "Course does not exist." });
-                }
-        
-                 res.status(201).json({
-                    message: "Course Deleted successfully.",
-
-                 })
-          
-      }
-      catch(error){
-          console.log("Error: ",error);
-          res.status(500).json({ message: "Internal Server Error", error: error.message });
-  
-          
-      }
-          
-      
-});
+courseRoute.delete('/remove/:id', courseAuth, deleteCourse);
 
 
 export { courseRoute }
